f9a7bf60a77404e5147a736239ec266f5ef5b3c3
Mark <stepanovsm8@gmail.com>
2022-12-02 15:03:41 +0000

    commit2

--- main.js
+++ main.js
@@ -1,25 +1,41 @@
 class Transformer {
-    constructor(name,img){
+    constructor(name,src){
         this.name = name
-        this.img = img
+        this.src = src
+    }
+
+    draw() {
+        if (!this.img){
+            this.img = document.createElement("img")
+            document.querySelector("div").appendChild(this.img)
+        }
+
+        if (!this.text){
+            this.text = document.createElement("p")
+            document.querySelector("div").appendChild(this.text)
+        }
+            
+        this.img.setAttribute("width","500px") 
+        this.img.setAttribute("heigth","500px") 
+        this.img.setAttribute("src",this.src)
+        
+        this.text.innerHTML = this.name
+        
+        
+        
+        
     }
 }
 
 const objects = []
 
-objects[0] = obj1 = new Transformer("Optimus Prime","https://static-asset-delivery.hasbroapps.com/a9e79c9b34ea183cad07eb995c5f51818b6c9447/7725af3d4fcc85f27b919beeb16ddc37.png")
-objects[1] = obj2 = new Transformer("Bumblebee", "https://static-asset-delivery.hasbroapps.com/a9e79c9b34ea183cad07eb995c5f51818b6c9447/d9448df28e3cbd747952a4abac005000.png")
-objects[2] = obj3 = new Transformer("Windblade", "https://static-asset-delivery.hasbroapps.com/a9e79c9b34ea183cad07eb995c5f51818b6c9447/250ac54040fbbebffeafb70e03a642f2.png")
+obj1 = new Transformer("Optimus Prime","https://static-asset-delivery.hasbroapps.com/a9e79c9b34ea183cad07eb995c5f51818b6c9447/7725af3d4fcc85f27b919beeb16ddc37.png")
+obj2 = new Transformer("Bumblebee", "https://static-asset-delivery.hasbroapps.com/a9e79c9b34ea183cad07eb995c5f51818b6c9447/d9448df28e3cbd747952a4abac005000.png")
+obj3 = new Transformer("Windblade", "https://static-asset-delivery.hasbroapps.com/a9e79c9b34ea183cad07eb995c5f51818b6c9447/250ac54040fbbebffeafb70e03a642f2.png")
 
-objects.forEach(obj => {
-    img = document.createElement("img")
-    img.setAttribute("width","500px") 
-    img.setAttribute("heigth","500px") 
-    img.setAttribute("src",obj.img)
-    text = document.createElement("p")
-    text.innerHTML = obj.name
-    
-    
-    document.querySelector("div").appendChild(img)
-    document.querySelector("div").appendChild(text)
-});
+obj1.draw()
+obj3.draw()
+obj2.draw()
+
+obj1.name = "lll"
+obj1.draw()
