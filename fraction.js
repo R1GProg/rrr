a6eb632ec176b747ed5f23abce7d808102e7e9ee
Mark <stepanovsm8@gmail.com>
2023-01-13 13:45:46 +0000

    Multiply

--- fraction.js
+++ fraction.js
@@ -1,1 +1,30 @@
-console.log("Hello");
+class Fraction {
+  constructor(numerator, denominator) {
+    this.numerator = numerator;
+    this.denominator = denominator;
+  }
+  toString(mixed = false) {
+    if (mixed) {
+      if (this.denominator != 0) {
+        const whole = Math.floor(this.numerator / this.denominator);
+        const numerator = this.numerator % this.denominator;
+        return `${whole} ${numerator}/${this.denominator}`;
+      }
+    }
+    return `${this.numerator}/${this.denominator}`;
+  }
+
+  mul(otherFraction) {
+    return new Fraction(
+      this.numerator * otherFraction.numerator,
+      this.denominator * otherFraction.denominator
+    ); 
+  }
+}
+
+const f = new Fraction(4, 3);
+const d = new Fraction(2, 4);
+console.log(`f = ${f}`);
+console.log(`d = ${d}`);
+//console.log(`${f.toString(true)}`);
+console.log(`f*d = ${f.mul(d)}`);
