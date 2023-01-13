a8afe5d161ec76fecb46c5273cc875500c4821b8
Gimbrint <reinis.bartkevics@gmail.com>
2023-01-13 16:41:43 +0200

    addeed simplifying

--- fraction.js
+++ fraction.js
@@ -15,20 +15,38 @@ class Fraction {
   }
   copy() {
     // return new object with the same num & denum
-    const m = new Fraction(this.numerator, this.denomerator);
+    const m = new Fraction(this.numerator, this.denomirator);
     return m;
   }
   div(other) {
     // Very half baked. Should probably have a way to simplify the resulting fraction. 10/5 -> 2/1
-    return new Fraction(this.numerator * this.denominator, this.denomerator * other.numerator);
+    return new Fraction(this.numerator * this.denominator, this.denomirator * other.numerator);
   }
+
+  simplify() {
+    // Example: 8/20 -> 2/5
+    const d = gcd(this.numerator, this.denominator);
+    this.numerator /= d;
+    this.denominator /= d;
+  }
+}
+
+function gcd(a, b) {
+  while (a != b) { 
+    if (a > b)
+      a = a - b;
+    else
+      b = b - a;
+  }
+  return a;
 }
 
 const f = new Fraction(4, 3);
 const a = f.copy();
 const d = new Fraction(2, 4);
 console.log(`f = ${f}`);
+d.simplify();
 console.log(`d = ${d}`);
 //console.log(`${f.toString(true)}`);
-console.log(`f*d = ${f.mul(d)}`);
+//console.log(`f*d = ${f.mul(d)}`);
 console.log(`a (f copy) = ${a}`);
