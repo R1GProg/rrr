8f53b6a0a62131dac0a1d82b25f30de32b7b3c9a
Keppler <kristaps.tikans@gmail.com>
2023-01-27 15:19:21 +0200

    GetSquareStateByAdress

--- chess.js
+++ chess.js
@@ -5,7 +5,20 @@ function getSquareState(board, posX, posY) {
 }
 
 function getSquareStateByAddress(board, pos) {
-
+  const text1 = text.split(text.charAt(1));
+	let posX = text1[0];
+	const text2 = text.split(text.charAt(0));
+	let posY = text2[1];
+	const alph[] = {A, B, C, D, E, F, G, H, I};
+	for(int i=0;i<alph.length();i++){
+		if(posY==alph[i]){
+			posY = i.toString();
+		}
+	}
+	if(board[posX-1][posY]!=null){
+		return board[posX-1][posY];
+	}
+	return "empty";
 }
 
 function setSquareStateByAddress(board, posX, posY, value) {
