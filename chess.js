ffb0043b5bb393a0417f0c23503c7727d77f9af1
Pavils Jurjans <passiday@gmail.com>
2023-01-27 15:53:12 +0200

    Add chess print.

--- chess.js
+++ chess.js
@@ -1,4 +1,4 @@
-const board = [[null, null, null],[null, null, null],[null, null, null]];
+const board = [[{type:'A'}, null, null],[null, {type:'B'}, null],[null, null, {type:'C'}]];
 
 function getSquareState(board, posX, posY) {
 
@@ -23,5 +23,16 @@ function movePiece(board, startPosX, startPosY, endPosX, endPosY) {
 }
 
 function printBoard(board) {
-  console.log("..T");
+  const sizeX = board.length;
+  const sizeY = board[0].length;
+  for (let y = 0; y < sizeY; y++) {
+    let row = '';
+    for (let x = 0; x < sizeX; x++) {
+      const squareState = board[x][sizeY - y - 1];
+      row += squareState ? squareState.type : '.';
+    }
+    console.log(row);
+  }
 }
+
+printBoard(board);
