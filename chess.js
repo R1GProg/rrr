958760cfec9d7f612a34375573f21bc3ad1cbcb6
Mark <stepanovsm8@gmail.com>
2023-01-27 16:49:27 +0200

    enumerateMoves function

--- chess.js
+++ chess.js
@@ -2,20 +2,19 @@ function initBoard(width, height) {
   let board = new Array(width);
 
   for (let x = 0; x < width; x++) {
-      board[x] = new Array(height);
+    board[x] = new Array(height);
 
-      for (let y = 0; y < height; y++) {
-          board[x][y] = null;
-      }
+    for (let y = 0; y < height; y++) {
+      board[x][y] = null;
+    }
   }
   return board;
 }
 
-function addInitialPieces(board) {
-}
+function addInitialPieces(board) {}
 
 function addPiece(board, x, y, player, type) {
-  board[x][y] = {player, type};
+  board[x][y] = { player, type };
 }
 
 function getSquareState(board, posX, posY) {
@@ -24,53 +23,108 @@ function getSquareState(board, posX, posY) {
 
 function getSquareStateByAddress(board, pos) {
   const text1 = text.split(text.charAt(1));
-	let posX = text1[0];
-	const text2 = text.split(text.charAt(0));
-	let posY = text2[1];
-	const alph = [A, B, C, D, E, F, G, H, I];
-	for(let i=0; i<alph.length(); i++){
-		if(posY==alph[i]){
-			posY = i.toString();
-		}
+  let posX = text1[0];
+  const text2 = text.split(text.charAt(0));
+  let posY = text2[1];
+  const alph = [A, B, C, D, E, F, G, H, I];
+  for (let i = 0; i < alph.length(); i++) {
+    if (posY == alph[i]) {
+      posY = i.toString();
+    }
   }
 
-	if(board[posX-1][posY]!=null){
-		return board[posX-1][posY];
-	}
-	return "empty";
+  if (board[posX - 1][posY] != null) {
+    return board[posX - 1][posY];
+  }
+  return "empty";
 }
 
-function setSquareStateByAddress(board, posX, posY, value) {
-
-}
-
-
+function setSquareStateByAddress(board, posX, posY, value) {}
 
 //let piece = {player:1|2, type:<string>}
 
-
 function movePiece(board, startPosX, startPosY, endPosX, endPosY) {
   // IF startPosX, startPosY, endPosX, endPosY ARE FROM 0e
-
 
   if (startPosX === endPosX || startPosY === endPosY) {
     console.log("can't move");
     return;
   }
-  if (board.length < (endPosX +1) || board[0].length < (endPosY+1)) {
-    console.log("can't move")
-    return
+  if (board.length < endPosX + 1 || board[0].length < endPosY + 1) {
+    console.log("can't move");
+    return;
   }
-  if (board.length < (startPosX +1) || board[0].length < (startPosY+1)){
-    console.log("start pos > board size")
-    return
+  if (board.length < startPosX + 1 || board[0].length < startPosY + 1) {
+    console.log("start pos > board size");
+    return;
   }
   board[endPosX][endPosY] = board[startPosX][startPosY];
   board[startPosX][startPosY] = null;
 }
 
 function enumerateMoves(board, x, y, piece) {
+  const moves = [];
   // Returns array of legal moves: [{x:int, y:int, capture:bool}]
+
+
+  
+
+  if (piece.type === "R") {
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 0, 1, false));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 0, -1, false));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, -1, 0, false));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 1, 0, false));
+  }
+
+  if (piece.type === "K"){
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 0, 1, true));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 0, -1, true));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, -1, 0, true));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 1, 0, true));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 1, 1, true));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 1, -1, true));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, -1, 1, true));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, -1, -1, true));
+  }
+
+  if (piece.type === "B"){
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 1, 1, false));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 1, -1, false));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, -1, 1, false));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, -1, -1, false));
+  }
+
+  if (piece.type === "Q"){
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 0, 1, false));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 0, -1, false));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, -1, 0, false));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 1, 0, false));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 1, 1, false));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 1, -1, false));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, -1, 1, false));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, -1, -1, false));
+  }
+
+  if (piece.type === "P"){
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 0, 1, true));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 1, 1, true));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, -1, 1, true));
+  }
+
+  if (piece.type === "N"){
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 2, 1, true));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 2, -1, true));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, -2, 1, true));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, -2, -1, true));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 1, 2, true));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, -1, 2, true));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, 1, -2, true));
+    moves.concat(enumerateMovesByDelta(board, x, y, piece.player, -1, -2, true));
+  }
+
+  return moves;
+
+  //byDelta = enumerateMovesByDelta(board, 4, 3, 1, 0, 1,null)
 }
 
 function enumerateMovesByDelta(board, x, y, player, dx, dy, onlyOnce) {
@@ -78,7 +132,7 @@ function enumerateMovesByDelta(board, x, y, player, dx, dy, onlyOnce) {
   const moves = [];
 
   while (true) {
-    moves.push({x:0, y:0, capture:false});
+    moves.push({ x: 0, y: 0, capture: false });
     return moves;
   }
 }
@@ -87,10 +141,10 @@ function printBoard(board) {
   const sizeX = board.length;
   const sizeY = board[0].length;
   for (let y = 0; y < sizeY; y++) {
-    let row = '';
+    let row = "";
     for (let x = 0; x < sizeX; x++) {
       const squareState = board[x][sizeY - y - 1];
-      row += squareState ? squareState.type : '.';
+      row += squareState ? squareState.type : ".";
     }
     console.log(row);
   }
@@ -99,8 +153,8 @@ function printBoard(board) {
 // Test code
 
 const board = initBoard(8, 8);
-addPiece(board, 4, 3, 1, 'R');
-addPiece(board, 4, 6, 2, 'K');
+addPiece(board, 4, 3, 1, "R");
+addPiece(board, 4, 6, 2, "K");
 printBoard(board);
 const rookUp = enumerateMovesByDelta(board, 4, 3, 1, 0, 1);
 /*
