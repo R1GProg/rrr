c68cc9fa652de405e89a7c6e2be74f2e93eaa336
Mark <stepanovsm8@gmail.com>
2023-01-27 15:42:35 +0200

    movePiece function

--- chess.js
+++ chess.js
@@ -46,13 +46,27 @@ function setSquareStateByAddress(board, posX, posY, value) {
 
 }
 
-function setSquareStateByAddress(board, pos, value) {
 
-}
 
 //let piece = {player:1|2, type:<string>}
 
+
 function movePiece(board, startPosX, startPosY, endPosX, endPosY) {
+  // IF startPosX, startPosY, endPosX, endPosY ARE FROM 0e
+
+
+  if (startPosX === endPosX || startPosY === endPosY) {
+    console.log("can't move");
+    return;
+  }
+  if (board.length < (endPosX +1) || board[0].length < (endPosY+1)) {
+    console.log("can't move")
+    return
+  }
+  if (board.length < (startPosX +1) || board[0].length < (startPosY+1)){
+    console.log("start pos > board size")
+    return
+  }
   board[endPosX][endPosY] = board[startPosX][startPosY];
   board[startPosX][startPosY] = null;
 }
