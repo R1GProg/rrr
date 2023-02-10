692d14bab8f305833c791a42dfe48c831770f699
Pavils Jurjans <passiday@gmail.com>
2023-02-10 16:57:58 +0200

    Join model and view.

---
+++ chessboard.js
@@ -0,0 +1,9 @@
+class Chessboard {
+  constructor(container) {
+    // TODO: build the chessboard HTML+CSS in the container
+    this.container = container;
+  }
+  update(model) {
+    console.log(model);
+  }
+}
