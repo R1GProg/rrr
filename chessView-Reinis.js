65528b9c3a90691e2b5a6b4b3d280ec2ff79972a
Gimbrint <reinis.bartkevics@gmail.com>
2023-03-24 15:53:19 +0200

    I need to sync now

--- chessView-Reinis.js
+++ chessView-Reinis.js
@@ -1,4 +1,25 @@
-class ChessboardR {
+class SimpleEvent {
+  constructor() {
+    this.eventHandlers = {};
+  }
+
+  addEventListener(eventType, eventHandler) {
+    // Create this type, if it doesn't allready exist!
+    if (!this.eventHandlers[eventHandler]) {
+      this.eventHandlers[eventType] = [];
+    }
+    
+    // We add a function (eventHandler) to the specified event type array
+    this.eventHandlers[eventType].push(eventHandler);
+  }
+    
+  dispatchEvents(eventType, eventInfo) {
+    // Call each function (eventHandler) of the specified event type and with the eventInfo parameter
+    this.eventHandlers[eventType].forEach(eh => eh(eventInfo));
+  }
+}
+
+class ChessboardR extends SimpleEvent {
   constructor(container) {
     // TODO: build the chessboard HTML+CSS in the container
     this.container = container;
@@ -8,12 +29,20 @@ class ChessboardR {
     };
   }
 
-  addEventListener(eventType, eventHandler) {
-    this.eventHandlers[eventType].push(eventHandler);
-  }
+  drawBoard() {
+    const board = document.getElementById('board')
 
-  triggerEvent(eventType, eventInfo) {
-    this.eventHandlers[eventType].forEach(eh => eh(eventInfo));
+    for (let x = 0; x < 8; x++) {
+      for (let y = 0; y < 8; y++) {
+        const xmlns = "http://www.w3.org/2000/svg";
+        cx = document.createElementNS(xmlns, "rect");
+        cx.setAttribute("x", 20+x*60);
+        cx.setAttribute("y", 20+y*60);
+        cx.setAttribute("height", 60);
+        cx.setAttribute("width", 60);
+        cx.setAttribute("class", "square");
+      }
+    }
   }
 
   move(txtMove) {
@@ -21,6 +50,8 @@ class ChessboardR {
   }
 
   update(model) {
+    
+    
   }
 
 }
